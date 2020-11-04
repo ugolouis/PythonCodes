bea3a4b962f94cdd9bdd1a35c444d59a8aad080f
ugolouis <louis_ul@yahoo.co.uk>
2020-11-04 22:30:11 +0000

    FilenamesChange

--- Pirple/Homework1_variables.py
+++ Pirple/Homework1_variables.py
@@ -1,10 +1,7 @@
 """ 
 Homework Assignment #1: variables
-
 List of attributes to describe a song using variables
 """
-
-
 # name of song
 songName = "We found love"
 # attributes of the song in variables
@@ -18,25 +15,21 @@ grammyAwardWinCartegory = "2013 Grammy award for best Music Video"
 producer ="calvin Harris"
 songVideoDirector = "Melina Matsoukas"
 
-"""
+
 # printing the variables
-print("Song name is: " + songName)
-print("Artist name: " + artist)
-print("Position in the chart on release: " + str(positionInChart))
-print("Date song was released: " + str(releaseDate))
-print("Gendre: " + gendre)
-print("Collaborating Artists: " + supportingArtist)
-print("Song album: " + album)
-print("Grammy award wining category: " + grammyAwardWinCartegory)
-print("Producer: " + producer)
-print("Song video producer: " + songVideoDirector)
 
-"""
-"""
-Chapter 2: Homework Assignment #2: Fuctions
+# print("Song name is: " + songName)
+# print("Artist name: " + artist)
+# print("Position in the chart on release: " + str(positionInChart))
+# print("Date song was released: " + str(releaseDate))
+# print("Gendre: " + gendre)
+# print("Collaborating Artists: " + supportingArtist)
+# print("Song album: " + album)
+# print("Grammy award wining category: " + grammyAwardWinCartegory)
+# print("Producer: " + producer)
+# print("Song video producer: " + songVideoDirector)
 
-"""
-
+#Chapter 2: Homework Assignment #2: Fuctions
 songName = "We found love"
 artist = "Rihana"
 positionInChart=1
