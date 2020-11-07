dcfbc11048a42e2a9e5f4e99fe62172e92316945
ugolouis <louis_ul@yahoo.co.uk>
2020-11-07 23:49:48 +0000

    commit

--- Pirple/Lesson8_participantData.py
+++ Pirple/Lesson8_participantData.py
@@ -8,33 +8,52 @@ Created by Louis
 
 # define variables we need
 
-registredparticipants = []
-outputFile = open("C:\\Users\LOzougwu\Desktop\W3Schools\participants.txt", "w")
-maxParticipants = 2
-incrementmentParticipants = 0
+# registredparticipants = []
+# outputFile = open("C:\\Users\LOzougwu\Desktop\W3Schools\participants.txt", "w")
+# maxParticipants = 2
+# incrementmentParticipants = 0
 
-while(incrementmentParticipants < maxParticipants):
-    participantData =  []
-    name = input("Please enter your name: ")
-    country = input("Please enter name of country: ")
-    age = input("Please enter your age: ")
-    participantData.append(name)
-    participantData.append(country)
-    participantData.append(str(age))
+# while(incrementmentParticipants < maxParticipants):
+#     participantData =  []
+#     name = input("Please enter your name: ")
+#     country = input("Please enter name of country: ")
+#     age = input("Please enter your age: ")
+#     participantData.append(name)
+#     participantData.append(country)
+#     participantData.append(str(age))
 
-    registredparticipants.append(participantData)
-    print(participantData)
+#     registredparticipants.append(participantData)
+#     print(participantData)
 
-    incrementmentParticipants += 1
-    print(registredparticipants)
+#     incrementmentParticipants += 1
+#     print(registredparticipants)
 
-for participant in registredparticipants:
+# for participant in registredparticipants:
+#     for data in participant:
+#         outputFile.write(data)
+#         outputFile.write(" ")
+#     outputFile.write("\n")
+     
+# outputFile.close()
 
-    for data in participant:
-        outputFile.write(data)
-        outputFile.write(" ")
-    outputFile.write("\n")
+# reimporting file into a dictionary
+
+age = {}
+
+inputFile = open("C:\\Users\LOzougwu\Desktop\W3Schools\participants.txt", "r")
+
+for line in inputFile:
+    tempvar = line.strip("\n"). split()
+    data = int(tempvar[-1])
+    if data in age:
+        age[data] += 1
+    else:
+        age[data] = 1
     
-    
-outputFile.close()
+print(age)
+
+inputFile.close()
+
+
+
 
